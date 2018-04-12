7a9be39aa35f9784d2ad4ee6df284b7c51865eab
https://bnelo12@github.com <s1623919@sms.ed.ac.uk>
2018-04-13 00:03:24 +0100

    Added edit contact menu

--- js/app.js
+++ js/app.js
@@ -2,10 +2,13 @@ var app = {
     create_org_menu_open: false,
     edit_org_menu_open: false,
     add_contact_menu_open: false,
+    edit_contact_menu_open: false,
     delete_org_menu_open: false,
     active_org: null,
+    active_contact: null,
     address_book: {
-        organizations: []
+        organizations: [],
+        contacts: []
     },
     update_ui: true
 }
@@ -60,6 +63,22 @@ var openDeleteOrgMenu = function() {
     });
 }
 
+var openEditContactMenu = function(name) {
+    closeAllSubMenus();
+    app.active_contact = getContactByName(name);
+    app.edit_contact_menu_open = true;
+    let phone = app.active_contact.phone;
+    if (phone != "") {
+        $("#edit-contact-form input")[0].value = app.active_contact.phone;
+        $("#edit-contact-form label")[0].innerText = "";
+    }
+    $('.edit-contact-view').velocity({
+        transform: "translate(50vw)"
+    }, {
+        easing: "swing",
+    });
+}
+
 var openEditOrgMenu = function(orgName) {
     closeAllMenus();
     app.active_org = getOrgByName(orgName);
@@ -67,7 +86,7 @@ var openEditOrgMenu = function(orgName) {
     $('#org-detail-phone').text("Phone: " + app.active_org.phone);
     $('#org-detail-email').text("Email: " + app.active_org.email);
     $('.organization-detail').velocity({
-        left: "25%"
+        transform: "translate(25vw)"
     }, {
         easing: "swing",
     });
@@ -79,7 +98,7 @@ var closeCreateOrgMenu = function() {
     $('#add-org-button-icon').text('add');
     clearCreateOrgMenu();
     $('.add-organization-view').velocity({
-        transform: "translate(0)"
+        transform: "translate(-25%)"
     }, {
         duration: 0
     });
@@ -106,7 +125,7 @@ var closeEditOrgMenu = function() {
     $('#contact-list').html("");
     $('#add-org-button-icon').text('add');
     $('.organization-detail').velocity({
-        left: "0"
+        transform: "translate(-25%)"
     }, {
         duration: 0
     });
@@ -116,6 +135,16 @@ var closeEditOrgMenu = function() {
 var closeDeleteOrgMenu = function() {
     app.delete_org_menu_open = false;
     $('.delete-org-view').velocity({
+        transform: "translate(0)"
+    }, {
+        duration: 0
+    });
+}
+
+var closeEditContactMenu = function() {
+    app.edit_contact_menu_open = false;
+    clearEditContactMenu();
+    $('.edit-contact-view').velocity({
         transform: "translate(0)"
     }, {
         duration: 0
@@ -162,6 +191,12 @@ var clearAddContactMenu = function() {
     $("#add-contact-form .mdl-textfield")[0].MaterialTextfield.change()
     $("#add-contact-form .mdl-textfield")[1].MaterialTextfield.change()
     $("#add-contact-form .mdl-textfield")[2].MaterialTextfield.change()
+}
+
+var clearEditContactMenu = function() {
+    $("#edit-contact-form")[0].reset();
+    $("#edit-contact-form .mdl-textfield")[0].MaterialTextfield.change()
+    $("#edit-contact-form .mdl-textfield")[1].MaterialTextfield.change()
 }
 
 var createContactJSON = function() {
@@ -233,16 +268,24 @@ var closeAllMenus = function() {
     if (app.edit_org_menu_open) closeEditOrgMenu();
     if (app.add_contact_menu_open) closeAddContactMenu();
     if (app.delete_org_menu_open) closeDeleteOrgMenu();
+    if (app.edit_contact_menu_open) closeEditContactMenu();
 }
 
 var closeAllSubMenus = function() {
     if (app.add_contact_menu_open) closeAddContactMenu();
     if (app.delete_org_menu_open) closeDeleteOrgMenu();
+    if (app.edit_contact_menu_open) closeEditContactMenu();
 }
 
 var getOrgByName = function(orgName) {
     return app.address_book.organizations.find(function(e) {
         return e.name == orgName;
+    });
+}
+
+var getContactByName = function(name) {
+    return app.address_book.contacts.find(function(e) {
+        return e.name == name;
     });
 }
 
@@ -270,9 +313,10 @@ var addContactToList = function(name, email, phone) {
     $(clone).find('#contact-name').text(name);
     $(clone).find('#contact-email').text(email);
     $(clone).find('#contact-phone').text(phone);
-    $(clone).find('li').attr('id', "contact-" + name);
+    let id = ("contact-" + name).replace(/\s/g, '');
+    $(clone).find('button').attr('id', id);
     let orgElem = $('#contact-list').append(clone);
-    // $("contact-" + name).click(() => {
-    //     openEditOrgMenu(name);
-    // });
+    $("#" + id).click(() => {
+        openEditContactMenu(name);
+    });
 }
